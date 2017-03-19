b3ec41f9c3f4adf6bfa0d0dfc79a654b6c50d041
Jason Lenoble <jason.lenoble@gmail.com>
2017-03-19 22:27:53 +0100

    Linted test file

--- test/destglob.test.js
+++ test/destglob.test.js
@@ -2,12 +2,11 @@ import path from 'path';
 import {expect} from 'chai';
 import destglob from '../src/destglob';
 
-describe('Testing destglob', function() {
-
+describe('Testing destglob', function () {
   const cwd = process.cwd();
 
   function title (glob) {
-    function func(glb) {
+    function func (glb) {
       return `'${glb}'`;
     }
 
@@ -18,107 +17,156 @@ describe('Testing destglob', function() {
     return func(glob);
   }
 
-  [
-    {
-      glob: 'index.js',
-      result: ['build/index.js']
-    },
-    {
-      glob: 'src/index.js',
-      result: ['build/src/index.js']
-    },
-    {
-      glob: 'src/*.js',
-      result: ['build/src/*.js']
-    },
-    {
-      glob: ['src/*.js'],
-      result: ['build/src/*.js']
-    },
-    {
-      glob: 'src/**/*.js',
-      result: ['build/src/**/*.js']
-    },
-    {
-      glob: ['src/*.js', 'test/**/*.js'],
-      result: ['build/src/*.js', 'build/test/**/*.js']
-    },
-    {
-      glob: [path.join(cwd, 'src/index.js')],
-      result: ['build/src/index.js']
-    },
-    {
-      glob: ['../src/*.js', '../test/**/*.js'],
-      result: ['src/*.js', 'test/**/*.js']
-    },
-    {
-      glob: ['src/../*.js', 'test/../**/*.js'],
-      result: ['build/*.js', 'build/**/*.js']
-    },
-    {
-      glob: ['gulp/*.js', '!gulp/clean.js'],
-      result: ['build/gulp/*.js', '!build/gulp/clean.js']
-    }
-  ].forEach(test => {
-
-    it(`Running destglob(${title(test.glob)}, 'build') is Ok`, function() {
-
-      expect(destglob(test.glob, 'build')).to.eql(test.result);
-
+  describe(`With dest as 'build', base as '.'`, function () {
+    [
+      {
+        glob: 'index.js',
+        result: ['build/index.js'],
+      },
+      {
+        glob: 'src/index.js',
+        result: ['build/src/index.js'],
+      },
+      {
+        glob: 'src/*.js',
+        result: ['build/src/*.js'],
+      },
+      {
+        glob: ['src/*.js'],
+        result: ['build/src/*.js'],
+      },
+      {
+        glob: 'src/**/*.js',
+        result: ['build/src/**/*.js'],
+      },
+      {
+        glob: ['src/*.js', 'test/**/*.js'],
+        result: ['build/src/*.js', 'build/test/**/*.js'],
+      },
+      {
+        glob: [path.join(cwd, 'src/index.js')],
+        result: ['build/src/index.js'],
+      },
+      {
+        glob: ['../src/*.js', '../test/**/*.js'],
+        result: ['src/*.js', 'test/**/*.js'],
+      },
+      {
+        glob: ['src/../*.js', 'test/../**/*.js'],
+        result: ['build/*.js', 'build/**/*.js'],
+      },
+      {
+        glob: ['gulp/*.js', '!gulp/clean.js'],
+        result: ['build/gulp/*.js', '!build/gulp/clean.js'],
+      },
+    ].forEach(test => {
+      it(`Running destglob(${title(test.glob)}, 'build') is Ok`, function () {
+        expect(destglob(test.glob, 'build')).to.eql(test.result);
+      });
     });
-
   });
 
-  [
-    {
-      glob: 'root/index.js',
-      result: ['root/build/index.js']
-    },
-    {
-      glob: 'root/src/index.js',
-      result: ['root/build/src/index.js']
-    },
-    {
-      glob: 'root/src/*.js',
-      result: ['root/build/src/*.js']
-    },
-    {
-      glob: ['root/src/*.js'],
-      result: ['root/build/src/*.js']
-    },
-    {
-      glob: 'root/src/**/*.js',
-      result: ['root/build/src/**/*.js']
-    },
-    {
-      glob: ['root/src/*.js', 'root/test/**/*.js'],
-      result: ['root/build/src/*.js', 'root/build/test/**/*.js']
-    },
-    {
-      glob: [path.join(cwd, 'root/src/index.js')],
-      result: ['root/build/src/index.js']
-    },
-    {
-      glob: ['../root/src/*.js', '../root/test/**/*.js'],
-      result: ['root/src/*.js', 'root/test/**/*.js']
-    },
-    {
-      glob: ['root/src/../*.js', 'root/test/../**/*.js'],
-      result: ['root/build/*.js', 'root/build/**/*.js']
-    },
-    {
-      glob: ['root/gulp/*.js', '!root/gulp/clean.js'],
-      result: ['root/build/gulp/*.js', '!root/build/gulp/clean.js']
-    }
-  ].forEach(test => {
-
-    it(`Running destglob(${title(test.glob)}, 'root/build', 'root') is Ok`,
-    function() {
-
-      expect(destglob(test.glob, 'root/build', 'root')).to.eql(test.result);
-
+  describe(`With dest as 'root/build', base as './root'`, function () {
+    [
+      {
+        glob: 'root/index.js',
+        result: ['root/build/index.js'],
+      },
+      {
+        glob: 'root/src/index.js',
+        result: ['root/build/src/index.js'],
+      },
+      {
+        glob: 'root/src/*.js',
+        result: ['root/build/src/*.js'],
+      },
+      {
+        glob: ['root/src/*.js'],
+        result: ['root/build/src/*.js'],
+      },
+      {
+        glob: 'root/src/**/*.js',
+        result: ['root/build/src/**/*.js'],
+      },
+      {
+        glob: ['root/src/*.js', 'root/test/**/*.js'],
+        result: ['root/build/src/*.js', 'root/build/test/**/*.js'],
+      },
+      {
+        glob: [path.join(cwd, 'root/src/index.js')],
+        result: ['root/build/src/index.js'],
+      },
+      {
+        glob: ['../root/src/*.js', '../root/test/**/*.js'],
+        result: ['root/src/*.js', 'root/test/**/*.js'],
+      },
+      {
+        glob: ['root/src/../*.js', 'root/test/../**/*.js'],
+        result: ['root/build/*.js', 'root/build/**/*.js'],
+      },
+      {
+        glob: ['root/gulp/*.js', '!root/gulp/clean.js'],
+        result: ['root/build/gulp/*.js', '!root/build/gulp/clean.js'],
+      },
+    ].forEach(test => {
+      it(`Running destglob(${title(test.glob)}, 'root/build', 'root') is Ok`,
+      function () {
+        expect(destglob(test.glob, 'root/build', 'root')).to.eql(test.result);
+      });
     });
-
   });
 
+  describe(`With dest as '/root', base as '.'`, function () {
+    [
+      {
+        glob: 'index.js',
+        result: ['/root/index.js'],
+      },
+      {
+        glob: 'src/index.js',
+        result: ['/root/src/index.js'],
+      },
+      {
+        glob: 'src/*.js',
+        result: ['/root/src/*.js'],
+      },
+      {
+        glob: ['src/*.js'],
+        result: ['/root/src/*.js'],
+      },
+      {
+        glob: 'src/**/*.js',
+        result: ['/root/src/**/*.js'],
+      },
+      {
+        glob: ['src/*.js', 'test/**/*.js'],
+        result: ['/root/src/*.js', '/root/test/**/*.js'],
+      },
+      {
+        glob: [path.join(cwd, 'src/index.js')],
+        result: ['/root/src/index.js'],
+      },
+      {
+        glob: ['../src/*.js', '../test/**/*.js'],
+        result: ['/src/*.js', '/test/**/*.js'],
+      },
+      {
+        glob: ['src/../*.js', 'test/../**/*.js'],
+        result: ['/root/*.js', '/root/**/*.js'],
+      },
+      {
+        glob: ['gulp/*.js', '!gulp/clean.js'],
+        result: ['/root/gulp/*.js', '!/root/gulp/clean.js'],
+      },
+      {
+        glob: ['../../../../tmp/gulpdest-1489942024029/src/**/*.js'],
+        result: ['/tmp/gulpdest-1489942024029/src/**/*.js'],
+      },
+    ].forEach(test => {
+      it(`Running destglob(${title(test.glob)}, '/root') is Ok`,
+      function () {
+        expect(destglob(test.glob, '/root')).to.eql(test.result);
+      });
+    });
+  });
 });
